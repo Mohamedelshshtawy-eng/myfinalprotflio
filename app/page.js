a009b2da93a4e44610937f95bbb42f996cc9ab86
Mohamed Elsheshtawy <mohamedelshshtawy-eng@gmail.com>
2026-05-02 15:23:27 +0300

    update the hero file

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@
 import React from "react";
 import LandPage from "./landpage/page";
 import Hero from "./pages/Hero";
-import Skills from "./pages/Skills";
+
 import ZoomSection from "./pages/ZoomSection";
 import About from "./pages/About";
-import Certifctas from "./pages/Certifctas";
+
 import Cursor from "./components/Cursor";
 import GlobalLoader from "./components/Loader";
 import Journey from "./pages/Journey";
